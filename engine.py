a8611f3224d831d7fadae1449733ba43f570d798
redV0ID <thousandkats@gmail.com>
2017-11-25 08:54:46 +0700

    new console

--- engine.py
+++ engine.py
@@ -12,11 +12,13 @@ def main():
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
     root_console = tdl.init(screen_width, screen_height,
                             title="Roguelike Tutorial Revised")
+    con = tdl.Console(screen_width, screen_height) # creating another console for drawing char on instd. of root
+    
     while not tdl.event.is_window_closed():
-        root_console.draw_char(player_x, player_y, '@', bg=None,
-                               fg=(255, 255, 255))
+        con.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
+        root_console.blit(con, 0, 0, screen_width ,screen_height, 0, 0) #draw con on root_root console
         tdl.flush()
-        root_console.draw_char(player_x, player_y, ' ', bg=None)
+        con.draw_char(player_x, player_y, ' ', bg=None)
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
@@ -35,15 +37,15 @@ def main():
         fullscreen = action.get('fullscreen')
 
         if move:
-        	dx, dy = move
-        	player_x += dx
-        	player_y += dy
+            dx, dy = move
+            player_x += dx
+            player_y += dy
 
         if exit:
-        	return True
+            return True
 
         if fullscreen:
-        	tdl.set_fullscreen(not tdl.get_fullscreen())
+            tdl.set_fullscreen(not tdl.get_fullscreen())
 
 if __name__ == "__main__":
     main()
