3f5683d62e3d5bfb7771a7ecacbb3fcf10a99dcd
redV0ID <thousandkats@gmail.com>
2017-11-25 10:08:46 +0700

    creating and renderong the map

--- engine.py
+++ engine.py
@@ -1,26 +1,39 @@
 import tdl
 from entity import Entity
 from input_handlers import handle_keys
+from render_functions import render_all, clear_all
+from map_utils import make_map
 
 def main():
 
     screen_width = 80
     screen_height = 50
-
+    map_width = 80
+    map_height = 45
+    # colors
+    colors = {
+        'dark_wall': (0, 0, 100),
+        'darl_ground': (50, 50, 150)
+    }
+    #objects
     player = Entity(int(screen_width/2), int(screen_height/2), '@', (255, 255, 255))
     npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', (255, 255, 0))
     entities = [player, npc]
-
+    #consoles and game map
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
     root_console = tdl.init(screen_width, screen_height,
                             title="Roguelike Tutorial Revised")
     con = tdl.Console(screen_width, screen_height) # creating another console for drawing char on instd. of root
-    
+
+    game_map = tdl.map.Map(map_width, map_height) # creating game map obj
+    make_map(game_map)
+
+
     while not tdl.event.is_window_closed():
-        con.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
-        root_console.blit(con, 0, 0, screen_width ,screen_height, 0, 0) #draw con on root_root console
+        #draw and clear on console
+        render_all(con, entities, game_map, root_console, screen_width, screen_height, colors)
         tdl.flush()
-        con.draw_char(player_x, player_y, ' ', bg=None)
+        clear_all(con, entities)
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
@@ -40,7 +53,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if game_map.walkable[player.x + dx, player.y + dy]:
+                player.move(dx, dy)
 
         if exit:
             return True
--- render_functions.py
+++ render_functions.py
@@ -1,9 +1,18 @@
-def render_all(con, entities, root_console, screen_width, screen_height):
+def render_all(con, entities, game_map, root_console, screen_width, screen_height, colors):
+	#Draw all the tiles in game map
+	for x, y in game_map:
+		wall = not game_map.transparent[x, y]
+
+		if wall:
+			con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'))
+		else:
+			con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'))
+
 	#Draw all entitites in th list
-	for entity in entitites:
+	for entity in entities:
 		draw_entitiy(con, entity)
 
-	root_console.blit(com, 0, 0, screen_width, screen_height, 0, 0)
+	root_console.blit(con, 0, 0, screen_width, screen_height, 0, 0)
 
 def clear_all(con, entitites):
 	for entity in entitites:
